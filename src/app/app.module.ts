7178a4922a38f6861f13e8c2a9c26ddc8de1d094
Felipe Neves <felipe.alneves@outlook.com>
2023-01-25 22:14:09 -0300

    Added default components and services for product

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddProductComponent } from './components/product/add-product/add-product.component';
+import { ProductDetailsComponent } from './components/product/product-details/product-details.component';
+import { ProductListComponent } from './components/product/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddProductComponent,
+    ProductDetailsComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
